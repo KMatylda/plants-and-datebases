131d23f0954fc0458e257fb3565e9b0d792acf06
Matylda <kingabartecka2@gmail.com>
2025-09-11 20:34:41 +0200

    Prepared a datebase

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 print("=========================")
 print("         Plants          ")
 print("=========================")
@@ -5,6 +7,7 @@ print("Choose what to do?")
 print("1. Search by name")
 print("2. Search by month")
 print("3. Input new flower data")
+print("4. Prepare database")
 print("Your choice:", end='')
 choice = input()
 if (choice == '1'):
@@ -13,5 +16,13 @@ elif (choice == '2'):
     print("Search month bla bla")
 elif (choice == '3'):
     print("Input new plant")
+elif (choice == '4'):
+    print("Preparing...")
+    connection = sqlite3.connect("plants.db")
+    cursor = connection.cursor()
+    connection.execute("BEGIN")
+    cursor.execute("CREATE TABLE plants(one text, two int)")
+    connection.commit()
+    print("Prepared successfully!")
 else:
     print("Wrong choice. Exiting!")
