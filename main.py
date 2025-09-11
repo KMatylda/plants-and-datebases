69da9ba46bec61db31396ed87da28c8f4bf6ea6a
Matylda <kingabartecka2@gmail.com>
2025-09-11 20:02:15 +0200

    initial draft of the code

---
+++ main.py
@@ -0,0 +1,17 @@
+print("=========================")
+print("         Plants          ")
+print("=========================")
+print("Choose what to do?")
+print("1. Search by name")
+print("2. Search by month")
+print("3. Input new flower data")
+print("Your choice:", end='')
+choice = input()
+if (choice == '1'):
+    print("Search bla bla")
+elif (choice == '2'):
+    print("Search month bla bla")
+elif (choice == '3'):
+    print("Input new plant")
+else:
+    print("Wrong choice. Exiting!")
